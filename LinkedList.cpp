a2820d87ed0991e75443c4a0dd18761d40237a66
Koichi Yazawa <kyazawa12@gmail.com>
2013-03-31 13:45:32 +0900

    ツリー構造の各ノードの値を再帰的に出力する関数 func を実装
    func の実装はいじらず、func が正常に稼動するよう LinkedList クラスに
    修正を加えていくこと。

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -416,9 +416,112 @@ char* LinkedList::toArray()
 
 
 
+void func(LinkedList* ll);
+
+
+
 int main()
 {
+  /***
+   * まずはリストを用意する
+   */
+  // 深さ0(ルート)
+  LinkedList* ll_root = new LinkedList();
 
+
+  // 深さ1
+  LinkedList* ll_l_dep1 = new LinkedList();
+  LinkedList* ll_r_dep1 = new LinkedList();
+
+
+  // 深さ2
+  LinkedList* ll_ll_dep2 = new LinkedList();
+  LinkedList* ll_lr_dep2 = new LinkedList();
+
+  LinkedList* ll_rl_dep2 = new LinkedList();
+  LinkedList* ll_rr_dep2 = new LinkedList();
+
+  
+  // 深さ3
+  LinkedList* ll_lll_dep3 = new LinkedList();
+  LinkedList* ll_llr_dep3 = new LinkedList();
+  LinkedList* ll_lrl_dep3 = new LinkedList();
+  LinkedList* ll_lrr_dep3 = new LinkedList();
+
+  LinkedList* ll_rll_dep3 = new LinkedList();
+  LinkedList* ll_rlr_dep3 = new LinkedList();
+  LinkedList* ll_rrl_dep3 = new LinkedList();
+  LinkedList* ll_rrr_dep3 = new LinkedList();
+  
+
+
+  /***
+   * 深いところにあるリストから値を詰め込んでいく
+   */
+  // 深さ3
+  ll_lll_dep3->addFirst('0');
+  ll_llr_dep3->addFirst('2');
+  ll_lrl_dep3->addFirst('2');
+  ll_lrr_dep3->addFirst('4');
+
+  ll_rll_dep3->addFirst('2');
+  ll_rlr_dep3->addFirst('4');
+  ll_rrl_dep3->addFirst('4');
+  ll_rrr_dep3->addFirst('6');
+
+
+  //   深さ2
+  ll_ll_dep2->addFirst('1');
+  ll_ll_dep2->addAll(1, ll_lll_dep3);
+  ll_ll_dep2->addAll(2, ll_llr_dep3);
+
+  ll_lr_dep2->addFirst('3');
+  ll_lr_dep2->addAll(1, ll_lrl_dep3);
+  ll_lr_dep2->addAll(2, ll_lrr_dep3);
+
+  ll_rl_dep2->addFirst('3');
+  ll_rl_dep2->addAll(1, ll_rll_dep3);
+  ll_rl_dep2->addAll(2, ll_rlr_dep3);
+
+  ll_rr_dep2->addFirst('5');
+  ll_rr_dep2->addAll(1, ll_rrl_dep3);
+  ll_rr_dep2->addAll(2, ll_rrr_dep3);
+
+
+  //   深さ1
+  ll_l_dep1->addFirst('2');
+  ll_l_dep1->addAll(1, ll_ll_dep2);
+  ll_l_dep1->addAll(2, ll_lr_dep2);
+
+  ll_r_dep1->addFirst('4');
+  ll_r_dep1->addAll(1, ll_rl_dep2);
+  ll_r_dep1->addAll(2, ll_rr_dep2);
+
+
+  //   深さ0
+  ll_root->addFirst('3');
+  ll_root->addAll(1, ll_l_dep1);
+  ll_root->addAll(2, ll_r_dep1);
+
+  
+  /***
+   * 各ノードの値を出力
+   */
+  func(ll_root);
+
+  
 
   return 0;
+}
+
+
+void func(LinkedList* ll) {
+  if ( ll->size() > 1)
+    {
+
+      func( (LinkedList*)(ll->get(1)) );
+      func( (LinkedList*)(ll->get(2)) );
+
+    }
+  printf("%d\n", ll->getFirst());
 }
